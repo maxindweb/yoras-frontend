1473af0dc500dd985531e28301c600695cf90307
maximiliian <johaneshamonangansilalahi@gmail.com>
2021-03-19 22:19:03 +0700

    init

---
+++ layouts/default.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <v-app-bar>
+      <v-img src="logo.png" width="102" height="93"></v-img>
+      <v-container>
+        <v-app-bar-title style="">
+          <h4>Paroki St.Yohanes Rasul</h4>
+          <h5>Pringwulung</h5>
+        </v-app-bar-title>
+      </v-container>
+
+      <v-spacer></v-spacer>
+      <v-btn text>Beranda</v-btn>
+      <v-btn text>Berita</v-btn>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on"> Renungan </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, index) in items" :key="index">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on"> Informasi </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, index) in items" :key="index">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on"> Tentang Kami </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, index) in items" :key="index">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-main>
+      <nuxt-child></nuxt-child>
+    </v-main>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire",
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js",
+    };
+  },
+};
+</script>
